a2be433ce382536ef02e0208aab698adf90557df
kritsapdev <kritsapdev@gmail.com>
2024-06-01 10:27:08 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,14 +4,25 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
-pipe = pipeline("text-generation", model="WizardLM/WizardMath-7B-V1.0")
-print(pipe("The goal in life is"))
+classifier = pipeline("sentiment-analysis")
+value = classifier(
+    [
+        "I've been waiting for a HuggingFace course my whole life.",
+        "I hate this so much!",
+    ]
+)
+
 
 
 @app.route('/')
 def index():
-  script_value = pipe("The goal in life is")
-  return render_template('index.html', script_value=script_value)
+  value = classifier(
+    [
+        "I've been waiting for a HuggingFace course my whole life.",
+        "I hate this so much!",
+    ]
+)
+  return render_template('index.html', script_value=value)
 
 if __name__ == '__main__':
   app.run(debug=True)
