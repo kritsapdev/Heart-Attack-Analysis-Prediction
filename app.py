d307085740ab46838458de49025e21139638810f
kritsapdev <kritsapdev@gmail.com>
2024-06-01 10:21:51 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask , render_template
+import torch
+from transformers import pipeline
 
 app = Flask(__name__)
 
+pipe = pipeline("text-generation", model="WizardLM/WizardMath-7B-V1.0")
+print(pipe("The goal in life is"))
+
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  script_value = pipe("The goal in life is")
+  return render_template('index.html', script_value=script_value)
 
 if __name__ == '__main__':
   app.run(debug=True)
+
+
+
