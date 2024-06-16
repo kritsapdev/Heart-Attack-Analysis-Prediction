6a188dc20d784e09bb8afed96ff6fc36304bede2
kritsapdev <kritsapdev@gmail.com>
2024-06-16 19:00:50 +0700

    1

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
 from flask import Flask , render_template
-import torch
-from transformers import pipeline
+
 
 app = Flask(__name__)
-
-classifier = pipeline("sentiment-analysis")
-value = classifier(
-    [
-        "I've been waiting for a HuggingFace course my whole life.",
-        "I hate this so much!",
-    ]
-)
-
 
 
 @app.route('/')
 def index():
-  value = classifier(
-    [
-        "I've been waiting for a HuggingFace course my whole life.",
-        "I hate this so much!",
-    ]
-)
-  return render_template('index.html', script_value=value)
+  return render_template('index.html',text = "Hello World")
 
 if __name__ == '__main__':
   app.run(debug=True)
